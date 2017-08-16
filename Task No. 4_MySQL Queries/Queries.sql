2cd206eb5c8a33654fa44eaebcaad0c5a64a0ffd
hazemkiwan <30780880+hazemkiwan@users.noreply.github.com>
2017-08-16 21:03:24 +0300

    Query

--- Task No. 4_MySQL Queries/Queries.sql
+++ Task No. 4_MySQL Queries/Queries.sql
@@ -2,4 +2,4 @@
 
 2. SELECT a.*, COUNT(c.article_id) AS 'comments_count' FROM articles a LEFT JOIN comments c ON a.id = c.article_id GROUP BY a.id ORDER BY comments_count DESC 
 
-3. SELECT MAX(name) FROM (SELECT u.*, COUNT(c.user_id) AS comments_count FROM users u LEFT JOIN comments c ON u.id = c.user_id GROUP BY u.id ORDER BY comments_count DESC) AS counts 
+3. SELECT u.*, COUNT(c.user_id) AS comments_count FROM users u LEFT JOIN comments c ON u.id = c.user_id GROUP BY u.id order by comments_count DESC LIMIT 1 
